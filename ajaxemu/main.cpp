f90da826d77d740a35093accc70959ad7287a02e
Ajax <ajaxshung@gmail.com>
2023-12-04 18:55:20 -0500

    Printing stack in sane order

--- ajaxemu/main.cpp
+++ ajaxemu/main.cpp
@@ -12,15 +12,16 @@
 // Width of integer registers in bits
 uint32_t const XLEN = 32;
 
-struct State
+// This is the state of just the processor
+typedef struct State
 {
-	// Program counter,
+	// Program counter
 	uint32_t pc;
 	
 	// Registers, they are called "x" in the technical document
 	// x[0] is just constant 0, and so we have 31 general purpose registers
 	uint32_t x[32];
-};
+} State;
 
 void initState(State& state)
 {
@@ -31,7 +32,7 @@ void initState(State& state)
 	state.pc = 0;
 }
 
-void runInstruction(State& state, uint32_t inst, uint8_t* memory)
+void runInstruction(State* state, uint32_t inst, uint8_t* memory)
 {	
 	// Step 1: Figure out instruction length
 	// Instructions come in 16 bit increments
@@ -57,13 +58,15 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 	// was designed to make easy (I think) because we get all the bits from the memory
 	// load at the same time.
 
+	// I matched variable names with how they are called in the ISA guide
+
 	// Normally this is the destination register, but in S and B type instructions
 	// where there is not destination register these same bits communicate parts of an immediate
 	// value. We always need to look at these bits as a unit no matter what
 	uint32_t rd = (inst >> 7) & 0x1f; // Bits 11 to 7
 
 	uint32_t opcode = inst & 0x7f;
-
+	
 	// I literally just put these in the order they are in as I read them from page 106 of the
 	// RISCV user guide version 2.2 lol
 	// There are certainly better ways to do this!
@@ -73,15 +76,15 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 		{
 			// We don't need to load it into low bits, then reshift it into high bits... can just read the bits in place!
 			// Lower bits are filled with zeros according to standard
-			state.x[rd] = inst & 0xfffff000;
-			state.pc += 4;
+			state->x[rd] = inst & 0xfffff000;
+			state->pc += 4;
 			break;
 		}
 		case 0x17: // auipc
 		{
 			// Mirrors the above, but result is imm + offset from pc
-			state.x[rd] = state.pc + (inst & 0xfffff000);
-			state.pc += 4;
+			state->x[rd] = state->pc + (inst & 0xfffff000);
+			state->pc += 4;
 			break;
 		}
 		case 0x6f: // jal
@@ -93,29 +96,29 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 			// this wants sign extension, we do it manually...
 			// also, yes, this is correct -- it doesn't set lsb
 			uint32_t imm = ((inst & (1 << 31)) >> 11) | ((inst & 0x7fe00000) >> 20) | ((inst & 0x00100000) >> 9) | (inst & 0x000ff000);
-			state.x[rd] = state.pc + 4;
+			state->x[rd] = state->pc + 4;
 			// Two cases: either our machine does sign extension and this is redundant, or it defaults to 0 extension and we need this
 			// No machine will default to 1 extension so we're all good
 			if(inst & (1 << 31))
 			{
 				imm |= 0xffe00000;
 			}
-			state.pc += imm;
+			state->pc += imm;
 			break;
 		}
 		case 0x67: // jalr
 		{
 			// This wants us to use a temporary in case the destination register and source register are the same
 			uint32_t rs1 = (inst >> 15) & 0x1f;
-			uint32_t temp = state.pc + 4;
+			uint32_t temp = state->pc + 4;
 			// Oh yeah we have to sign this one again, but bits are nicer, [11:0], so 31 -> 11 == 20
 			uint32_t imm = (inst >> 20);
 			if(inst & (1 << 31))
 			{
 				imm |= 0xfffff000;
 			}
-			state.pc = (state.x[rs1] + imm) & ~1;
-			state.x[rd] = temp;
+			state->pc = (state->x[rs1] + imm) & ~1;
+			state->x[rd] = temp;
 			break;
 		}
 		case 0x63: // beq, bne, blt, bge, bltu, bgeu
@@ -135,37 +138,37 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 			{
 				case 0x0: // beq
 				{
-					if(state.x[rs1] == state.x[rs2]) { state.pc += imm; }
+					if(state->x[rs1] == state->x[rs2]) { state->pc += imm; }
 					break;
 				}
 				case 0x1: // bne
 				{
-					if(state.x[rs1] != state.x[rs2]) { state.pc += imm; }
+					if(state->x[rs1] != state->x[rs2]) { state->pc += imm; }
 					break;
 				}
 				case 0x4: // blt (this is signed)
 				{
-					if((int32_t)state.x[rs1] < (int32_t)state.x[rs2]) { state.pc += imm; }
+					if((int32_t)state->x[rs1] < (int32_t)state->x[rs2]) { state->pc += imm; }
 					break;
 				}
 				case 0x5: // bge (this is signed)
 				{
-					if((int32_t)state.x[rs1] >= (int32_t)state.x[rs2]) { state.pc += imm; }
+					if((int32_t)state->x[rs1] >= (int32_t)state->x[rs2]) { state->pc += imm; }
 					break;
 				}
 				case 0x6: // bltu (this is unsigned)
 				{
-					if((uint32_t)state.x[rs1] < (uint32_t)state.x[rs2]) { state.pc += imm; }
+					if((uint32_t)state->x[rs1] < (uint32_t)state->x[rs2]) { state->pc += imm; }
 					break;
 				}
 				case 0x7: // bgeu (this is unsigned)
 				{
-					if((uint32_t)state.x[rs1] >= (uint32_t)state.x[rs2]) { state.pc += imm; }
+					if((uint32_t)state->x[rs1] >= (uint32_t)state->x[rs2]) { state->pc += imm; }
 					break;
 				}
 				// TODO: handle if it isn't one of these? Set trap maybe?
 			}
-			state.pc += 4;
+			state->pc += 4;
 			break;
 		}
 		case 0x03: // lb, lh, lw, lbu, lhu
@@ -182,36 +185,36 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 			{
 				case 0x0: // lb
 				{
-					uint8_t loaded = *(uint8_t*)(memory + (state.x[rs1] + imm));
-					state.x[rd] = (loaded & (1 << 7)) ? loaded | 0xffffff00 : loaded;
+					uint8_t loaded = *(uint8_t*)(memory + (state->x[rs1] + imm));
+					state->x[rd] = (loaded & (1 << 7)) ? loaded | 0xffffff00 : loaded;
 					break;
 				}
 				case 0x1: // lh
 				{
-					uint16_t loaded = *(uint16_t*)(memory + (state.x[rs1] + imm));
-					state.x[rd] = (loaded & (1 << 15)) ? loaded | 0xffff0000 : loaded;
+					uint16_t loaded = *(uint16_t*)(memory + (state->x[rs1] + imm));
+					state->x[rd] = (loaded & (1 << 15)) ? loaded | 0xffff0000 : loaded;
 					break;
 				}
 				case 0x2: // lw
 				{
-					state.x[rd] = *(uint32_t*)(memory + (state.x[rs1] + imm));
+					state->x[rd] = *(uint32_t*)(memory + (state->x[rs1] + imm));
 					break;
 				}
 				case 0x4: // lbu
 				{
-					uint8_t loaded = *(uint8_t*)(memory + (state.x[rs1] + imm));
-					state.x[rd] = loaded & 0x000000ff;
+					uint8_t loaded = *(uint8_t*)(memory + (state->x[rs1] + imm));
+					state->x[rd] = loaded & 0x000000ff;
 					break;
 				}
 				case 0x5: // lhu
 				{
-					uint16_t loaded = *(uint16_t*)(memory + (state.x[rs1] + imm));
-					state.x[rd] = loaded & 0x0000ffff;
+					uint16_t loaded = *(uint16_t*)(memory + (state->x[rs1] + imm));
+					state->x[rd] = loaded & 0x0000ffff;
 					break;
 				}
 				// TODO: handle if it isn't one of these? Set trap maybe?
 			}
-			state.pc += 4;
+			state->pc += 4;
 			break;
 		}
 		case 0x23: // sb, sh, sw
@@ -229,22 +232,22 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 			{
 				case 0x0: // sb
 				{
-					*(uint8_t*)(memory + (state.x[rs1] + imm)) = state.x[rs2];
+					*(uint8_t*)(memory + (state->x[rs1] + imm)) = state->x[rs2];
 					break;
 				}
 				case 0x1: // sh
 				{
-					*(uint16_t*)(memory + (state.x[rs1] + imm)) = state.x[rs2];
+					*(uint16_t*)(memory + (state->x[rs1] + imm)) = state->x[rs2];
 					break;
 				}
 				case 0x2: // sw
 				{
-					*(uint32_t*)(memory + (state.x[rs1] + imm)) = state.x[rs2];
+					*(uint32_t*)(memory + (state->x[rs1] + imm)) = state->x[rs2];
 					break;
 				}
 				// TODO: handle default?
 			}
-			state.pc += 4;
+			state->pc += 4;
 			break;
 		}
 		case 0x13: // addi, slti, sltiu, xori, ori, andi, slli, srli, srai
@@ -260,34 +263,34 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 			{
 				case 0x0: // addi
 				{
-					state.x[rd] = state.x[rs1] + imm;
+					state->x[rd] = state->x[rs1] + imm;
 					break;
 				}
 				case 0x2: // slti
 				{
 					// I'm pretty sure c standard says true statements always get set to 1 but just to make
 					// it clear
-					state.x[rd] = ((int32_t)state.x[rs1] < (int32_t)imm) ? 1 : 0;
+					state->x[rd] = ((int32_t)state->x[rs1] < (int32_t)imm) ? 1 : 0;
 					break;
 				}
 				case 0x3: // sltiu
 				{
-					state.x[rd] = ((uint32_t)state.x[rs1] < (uint32_t)imm) ? 1 : 0;
+					state->x[rd] = ((uint32_t)state->x[rs1] < (uint32_t)imm) ? 1 : 0;
 					break;
 				}
 				case 0x4: // xori
 				{
-					state.x[rd] = state.x[rs1] ^ imm;
+					state->x[rd] = state->x[rs1] ^ imm;
 					break;
 				}
 				case 0x6: // ori
 				{
-					state.x[rd] = state.x[rs1] | imm;
+					state->x[rd] = state->x[rs1] | imm;
 					break;
 				}
 				case 0x7: // andi
 				{
-					state.x[rd] = state.x[rs1] & imm;
+					state->x[rd] = state->x[rs1] & imm;
 					break;
 				}
 				case 0x1: // slli
@@ -295,7 +298,7 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 					// TODO: these instructions only use the lowest 5 bits of imm, and
 					// the standard says the high bits are all 0 (or 1 of them is 1 for srai)
 					// I assume it should be illegal operation if that's not the case?
-					state.x[rd] = state.x[rs1] << (imm & 0x1f);
+					state->x[rd] = state->x[rs1] << (imm & 0x1f);
 					break;
 				}
 				case 0x5: // srli, srai are differentiated by a 1 in the 30th bit
@@ -303,23 +306,23 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 					uint32_t shamt = imm & 0x1f;
 					if(inst & (1 << 30))
 					{
-						state.x[rd] = (int32_t)(state.x[rs1]) >> shamt;
-						if((state.x[rs1] & (1 << 31)) && shamt)
+						state->x[rd] = (int32_t)(state->x[rs1]) >> shamt;
+						if((state->x[rs1] & (1 << 31)) && shamt)
 						{
 							// Bit shifts by 32 are undefined by c standard so we actually can't use this which is extremely cringe
 							// because it won't work on 0 shift... so we just special case it. 
-							state.x[rd] |= ~0 << (32 - shamt);
+							state->x[rd] |= ~0 << (32 - shamt);
 						}
 					}
 					else
 					{
 						// Don't do sign extension (don't need to do anything special here)
-						state.x[rd] = (uint32_t)(state.x[rs1]) >> shamt;
+						state->x[rd] = (uint32_t)(state->x[rs1]) >> shamt;
 					}
 					break;
 				}
 			}
-			state.pc += 4;
+			state->pc += 4;
 			break;
 		}
 		case 0x33: // add, sub, sll, slt, sltu, xor, srl, sra, or, and
@@ -334,76 +337,76 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 					// This isn't 122
 					if(inst & (1 << 30)) // add
 					{
-						state.x[rd] = state.x[rs1] + state.x[rs2];
+						state->x[rd] = state->x[rs1] + state->x[rs2];
 					}
 					else // sub
 					{
-						state.x[rd] = state.x[rs1] - state.x[rs2];
+						state->x[rd] = state->x[rs1] - state->x[rs2];
 					}
 					break;
 				}
 				case 0x1: // sll
 				{
 					// This only cares about the lower 5 bits
-					state.x[rd] = state.x[rs1] << (state.x[rs2] & 0x1f);
+					state->x[rd] = state->x[rs1] << (state->x[rs2] & 0x1f);
 					break;
 				}
 				case 0x2: // slt
 				{
-					state.x[rd] = ((int32_t)state.x[rs1] < (int32_t)state.x[rs2]) ? 1 : 0;
+					state->x[rd] = ((int32_t)state->x[rs1] < (int32_t)state->x[rs2]) ? 1 : 0;
 					break;
 				}
 				case 0x3: // sltu
 				{
-					state.x[rd] = ((uint32_t)state.x[rs1] < (uint32_t)state.x[rs2]) ? 1 : 0;
+					state->x[rd] = ((uint32_t)state->x[rs1] < (uint32_t)state->x[rs2]) ? 1 : 0;
 					break;
 				}
 				case 0x4: // xor
 				{
-					state.x[rd] = state.x[rs1] ^ state.x[rs2];
+					state->x[rd] = state->x[rs1] ^ state->x[rs2];
 					break;
 				}
 				case 0x5: // srl, sra
 				{
-					uint32_t shamt = state.x[rs2] & 0x1f;
+					uint32_t shamt = state->x[rs2] & 0x1f;
 					if(inst & (1 << 30)) 
 					{
-						state.x[rd] = (int32_t)(state.x[rs1]) >> shamt;
-						if(state.x[rs1] & (1 << 31) && shamt)
+						state->x[rd] = (int32_t)(state->x[rs1]) >> shamt;
+						if(state->x[rs1] & (1 << 31) && shamt)
 						{
-							state.x[rd] |= ~0 << (32 - shamt);
+							state->x[rd] |= ~0 << (32 - shamt);
 						}
 					}
 					else
 					{
-						state.x[rd] = (uint32_t)(state.x[rs1]) >> shamt;
+						state->x[rd] = (uint32_t)(state->x[rs1]) >> shamt;
 					}
 					break;
 				}
 				case 0x6: // or
 				{
-					state.x[rd] = state.x[rs1] | state.x[rs2];
+					state->x[rd] = state->x[rs1] | state->x[rs2];
 					break;
 				}
 				case 0x7: // and
 				{
-					state.x[rd] = state.x[rs1] & state.x[rs2];
+					state->x[rd] = state->x[rs1] & state->x[rs2];
 					break;
 				}
 			}
-			state.pc += 4;
+			state->pc += 4;
 			break;
 		}
 		case 0x0f: // fence, fence.i
 		{
 			// TODO: do something other than nop?
-			state.pc += 4;
+			state->pc += 4;
 			break;
 		}
 		case 0x73: // ecall, ebreak, csrrw, csrrs, csrrc, csrrwi, csrrsi, csrrci
 		{
 			// TODO: do something other than nop?
-			state.pc += 4;
+			state->pc += 4;
 			break;
 		}
 	}
@@ -411,7 +414,7 @@ void runInstruction(State& state, uint32_t inst, uint8_t* memory)
 	// We could have written to 0, so just put it back to 0
 	if(rd == 0) 
 	{
-		state.x[rd] = 0;
+		state->x[rd] = 0;
 	}
 }
 
@@ -437,7 +440,7 @@ int main(int argc, char** argv)
 	uint8_t* memory = nullptr;
 	uint8_t* program = nullptr;
 
-	uint32_t memorySize = 0xff;
+	uint32_t memorySize = 256;
 	memory = (uint8_t*)malloc(memorySize + programSize);
 	if(!memory)
 	{
@@ -461,7 +464,7 @@ int main(int argc, char** argv)
 	{
 		uint32_t inst = *(uint32_t*)(program + state.pc);
 		printf("executing instruction: %08x\n", inst);
-		runInstruction(state, inst, memory);
+		runInstruction(&state, inst, memory);
 		printf("pc = %u\n", state.pc);
 		if(state.pc == DONE_ADDRESS)
 		{
@@ -476,7 +479,7 @@ int main(int argc, char** argv)
 		{
 			printf("\n");
 		}
-		printf("%02x ", *(uint8_t*)(memory + i));
+		printf("%02x ", *(uint8_t*)(memory + memorySize - i - 1));
 	}
 	printf("\n");
 
