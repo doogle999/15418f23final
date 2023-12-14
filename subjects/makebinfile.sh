f81c6702e82fc2defa4cfbad57ea53d19a5699be
Ajax <ajaxshung@gmail.com>
2023-12-14 08:30:35 -0500

    Fixed entry point

--- subjects/makebinfile.sh
+++ subjects/makebinfile.sh
@@ -5,13 +5,13 @@ baseFile=${sourceFile%%.*}
 
 riscv32-unknown-linux-gnu-gcc -c -fPIC ${sourceFile} -o ${baseFile}.o -march=rv32id -mabi=ilp32d
 # Sanity
-# riscv32-unknown-linux-gnu-objdump -m riscv -D ${baseFile}.o
+riscv32-unknown-linux-gnu-objdump -m riscv -D ${baseFile}.o
 
 riscv32-unknown-linux-gnu-ld ${baseFile}.o -o ${baseFile}.ln
 
 # Find the entry point
-riscv32-unknown-linux-gnu-nm  ${baseFile}.ln | sed -nr 's/([0-9]{8}) T main/\1/p' > ${baseFile}.entry
+riscv32-unknown-linux-gnu-nm  ${baseFile}.o | sed -nr 's/([a-f0-9]{8}) T main/\1/p' > ${baseFile}.entry
 
 riscv32-unknown-linux-gnu-objcopy -O binary -j .text ${baseFile}.ln ${baseFile}.bin
 # Sanity
-# riscv32-unknown-linux-gnu-objdump -m riscv -b binary -D ${baseFile}.bin
+riscv32-unknown-linux-gnu-objdump -m riscv -b binary -D ${baseFile}.bin
