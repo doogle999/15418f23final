77df3b2cc34d64d050fc67e002affe60e99181b8
Ajax <ajaxshung@gmail.com>
2023-12-14 17:39:06 -0500

    Made compare

--- subjects/compare/main.c
+++ subjects/compare/main.c
@@ -6,10 +6,59 @@ int compare(char const* a, char const* b)
 
 int main(int argc, char** argv)
 {
-    if(argc != 3)
+	if(argc != 2)
 	{
 		return -1;
 	}
 
-	return compare(argv[1], argv[2]);
+	if(compare(argv[1], "hello") >= 0)
+	{
+		if(compare(argv[1], "zaz") >= 0)
+		{
+			if(compare(argv[1], "zzz") >= 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return 1;
+			}
+		}
+		else
+		{
+			if(compare(argv[1], "sus") >= 0)
+			{
+				return 2;
+			}
+			else
+			{
+				return 3;
+			}
+		}
+	}
+	else
+	{
+		if(compare(argv[1], "bca") >= 0)
+		{
+			if(compare(argv[1], "gull") >= 0)
+			{
+				return 4;
+			}
+			else
+			{
+				return 5;
+			}
+		}
+		else
+		{
+			if(compare(argv[1], "aca") >= 0)
+			{
+				return 6;
+			}
+			else
+			{
+				return 7;
+			}
+		}
+	}
 }
