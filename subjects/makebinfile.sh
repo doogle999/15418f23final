e9acd30d40ab29918f80eadaf3a1f9951c7afc86
Ajax <ajaxshung@gmail.com>
2023-12-14 08:09:52 -0500

    Fixed not calling linker...

--- subjects/makebinfile.sh
+++ subjects/makebinfile.sh
@@ -3,13 +3,15 @@
 sourceFile=$1
 baseFile=${sourceFile%%.*}
 
-riscv32-unknown-linux-gnu-gcc -c ${sourceFile} -o ${baseFile}.o -march=rv32id -mabi=ilp32d
+riscv32-unknown-linux-gnu-gcc -c -fPIC ${sourceFile} -o ${baseFile}.o -march=rv32id -mabi=ilp32d
 # Sanity
 # riscv32-unknown-linux-gnu-objdump -m riscv -D ${baseFile}.o
 
-# Find the entry point
-riscv32-unknown-linux-gnu-nm  ${baseFile}.o | sed -nr 's/([0-9]{8}) T main/\1/p' > ${baseFile}.entry
+riscv32-unknown-linux-gnu-ld ${baseFile}.o -o ${baseFile}.ln
 
-riscv32-unknown-linux-gnu-objcopy -O binary -j .text ${baseFile}.o ${baseFile}.bin
+# Find the entry point
+riscv32-unknown-linux-gnu-nm  ${baseFile}.ln | sed -nr 's/([0-9]{8}) T main/\1/p' > ${baseFile}.entry
+
+riscv32-unknown-linux-gnu-objcopy -O binary -j .text ${baseFile}.ln ${baseFile}.bin
 # Sanity
-#riscv32-unknown-linux-gnu-objdump -m riscv -b binary -D ${baseFile}.bin
+# riscv32-unknown-linux-gnu-objdump -m riscv -b binary -D ${baseFile}.bin
